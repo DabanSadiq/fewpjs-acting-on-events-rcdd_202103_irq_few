e718c957b06293165eb603db87863bcc410b7baa
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 12:40:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 console.log('w')
-  if (right < 180) {
+  if (right > 180) {
     dodger.style.right = `${right - 1}px`;
   }
 }
