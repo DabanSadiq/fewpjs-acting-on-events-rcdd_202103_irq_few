d578b49c3caf597789006b2ee633fc85a61bc81c
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 12:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ document.addEventListener("keydown", function(e) {
   }
 });
 
-unction moveDodgerRight() {
+function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
